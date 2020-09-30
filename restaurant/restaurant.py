0df89bd879155e22bff6e947ad8808963007b9d7
PoulStar Student <pstudent@localhost.localdomain>
2020-09-30 17:30:19 +0330

    jsjjs

--- restaurant/restaurant.py
+++ restaurant/restaurant.py
@@ -12,6 +12,11 @@ i = {
          "review":128,
          "price":2,
          "img":"restaurant/images/e.gif"},
+    "2":{"name": "akbar chicken",
+         "rating":5,
+         "review":128,
+         "price":2,
+         "img":"restaurant/images/e.gif"},
 }
 
 root = tk.Tk()
@@ -28,20 +33,21 @@ note.add(drink, text="Drinks")
 note.add(rescipt, text="Rescipt")
 
 # ###################foods################### #
-f1 = tk.Frame(food,bg='#64ffda')
-f1.grid(row=0, column=0)
+for j in range(len(i)):
+    f1 = tk.Frame(food,bg='#64ffda')
+    f1.grid(row=j, column=0)
 
-name = i["1"]["name"]
-tk.Label(f1, text=name,bg="#ff3d00").grid(row=0, column=0)
+    name = i["1"]["name"]
+    tk.Label(f1, text=name,bg="#ff3d00").grid(row=0, column=0)
 
-rating = i["1"]["rating"] * "★" + "("+str(i["1"]["review"])+")"
-tk.Label(f1,text=rating,cnf=bgc).grid(row=1,column=0)
+    rating = i["1"]["rating"] * "★" + "("+str(i["1"]["review"])+")"
+    tk.Label(f1,text=rating,cnf=bgc).grid(row=1,column=0)
 
-price = str(i["1"]["rating"]) +"$"
-tk.Label(f1,text=price,cnf=bgc).grid(row=2,column=0)
+    price = str(i["1"]["rating"]) +"$"
+    tk.Label(f1,text=price,cnf=bgc).grid(row=2,column=0)
 
-img = PhotoImage(file=i["1"]["img"]).subsample(2)
-tk.Label(f1,image=img).grid(row=0,column=1,rowspan=3)
+    img = PhotoImage(file=i["1"]["img"]).subsample(2)
+    tk.Label(f1,image=img).grid(row=0,column=1,rowspan=3)
 ####################
 
 
