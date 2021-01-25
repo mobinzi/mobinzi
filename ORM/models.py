b13abf9d49840058c20a8b7f5340a056521eb505
Your Name <you@example.com>
2021-01-25 17:26:54 +0330

    models

--- ORM/models.py
+++ ORM/models.py
@@ -1,19 +1,23 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import column,Integer,String,Date,DateTime,TIMESTAMP
+from connection import connection
+from sqlalchemy.sql.functions import func
+
+Base = declarative_base
+
 class student(Base):
-    __tablename__="student"
-    studentId= Column(Integer(),primary_key=True,autooincrement=True)
-    name= column(String(50))
-    family= Column(String(50))
-    grade=
+    __tablename__ = "student"
+    studentId =column(Integer,primary_key=True,autoincrement=True)
+    first_name = column(String(255))
+    last_name = column(String(255))
+    birth_date = column(Date) 
+    grade = column(Integer) 
+    level = column(Integer)
+    updated_at = column(TIMESTAMP,server_default=func.now(),onupdate=func.current_timestamp)
 
-    def __init__(self,name=None,family=None,grade=None):
-    self.name = name
-    self.family = family
-    self.grade = grade
-
-engine= Connection().get_connection()
-if not engine.dailect.has_table(engine,"student"):
-    Base.metadata.create_all(engine,checkfirst)=True)
-    print("Database Created")
-
-else:
-    print("Database{} exist!".format("student"))    
+def __init__(self,f_n,l_n,b_d,g,l):
+    self.first_name = f_n
+     self.last_name = l_n
+     self.birth_date = b_d
+     self.grade = g
+     self.level = l
