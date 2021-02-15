3c05b357a4cce684ec257234b93e3fdea0d4b83e
Your Name <you@example.com>
2021-02-15 17:04:14 +0330

    fsdfjs

--- club/gui.py
+++ club/gui.py
@@ -1,14 +1,33 @@
 import tkinter as tk
+from tkinter.filedialog import askopenfilename
+from tkcalendar import DateEntry
 
 
 class GUI(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
-        tk.Label(self,text="First name").grid(row=0,column=0)
-        name = tk.StringVar()
-        tk.Entry(self).grid(row=0,column=1)
         
+        tk.Label(self,text="First name").grid(row=0,column=0)
+        self.name = tk.StringVar()
+        tk.Entry(self, textvariable=self.name).grid(row=0,column=1)
+        
+        tk.Label(self,text="Last name").grid(row=1,column=0)
+        self.last = tk.StringVar()
+        tk.Entry(self,textvariable=self.last).grid(row=1,column=1)
+        tk.Button(self,text="Create",command=self.create).grid(row=2,column=1)
 
+        tk.Label(self,text="birth").grid(row=2,column=0)
+        self.birth = tk.StringVar()        
+        DateEntry(self,textvariable=self.birth,date_patttern="y-mm-dd",background="darkblue",foreground="white").grid(row=2,column=1)
+
+
+    def create(self):
+        print(self.name.get(),self.last.get())
+
+
+
+
+        
     def main(self):
         self.mainloop()
 
